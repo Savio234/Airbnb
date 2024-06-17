0eb3b0d587b7d788b981669ef76dd9ed591cb300
Dominic <dominicezeifejafor@gmail.com>
2024-06-17 16:24:38 +0100

    Building out modals

--- shared/modals/RegisterModal/RegisterModal.tsx
+++ shared/modals/RegisterModal/RegisterModal.tsx
@@ -1,15 +1,41 @@
 'use client';
 import React, { useState, useCallback } from 'react'
 import axios from 'axios';
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { RegisterModalProps } from '@/interface/modals';
 import { ModalElement } from '@/shared';
 
+interface FormDataProps {
+  name: string
+  email: string
+  password: string
+}
 const RegisterModal = ({isOpen, onClose, onSubmit}: RegisterModalProps) => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const signupSchema = yup.object().shape({
+    name: yup.string().required('This field is required'),
+    email: yup.string().required('Email is required'),
+    password: yup.string().min(8, 'Must be at least 8 characters').required('Password is required')
+  })
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+    // resolver: yupResolver(signupSchema)
+  })
+
+  const handleRegister: SubmitHandler<FieldValues> = (data: any) => {
+    setLoading(true)
+  }
+
   return (
-    <ModalElement isOpen={isOpen} title='Sign Up' onClose={onClose} onSubmit={onSubmit}>
+    <ModalElement actionLabel='Register' isOpen={isOpen} title='Sign Up' onClose={onClose} onSubmit={onSubmit}>
 
     </ModalElement>
   )
