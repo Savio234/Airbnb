2d157c8f10dcacf1324db895aad3efa5f7802bf5
Dominic <dominicezeifejafor@gmail.com>
2024-06-27 17:18:51 +0100

    Responsiveness

--- shared/MenuItem/MenuItem.tsx
+++ shared/MenuItem/MenuItem.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react'
 import { MenuItemProps } from '@/interface';
+import styles from './MenuItem.module.css'
 
 const MenuItem = ({onclick, label}: MenuItemProps) => {
   return (
-    <div onClick={onclick} className='p-[1.6rem] text-[2rem] hover: bg-neutral-100 transition semi-bold'>
+    <div onClick={onclick} className={`${styles.menu_item} p-[1.6rem] text-[2rem] hover: bg-neutral-100 transition semi-bold`}>
       {label}
     </div>
   )
