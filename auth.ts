30515f2ed06aba9afad683dc32adea064c60004a
Dominic <dominicezeifejafor@gmail.com>
2024-08-08 17:54:04 +0100

    Changes

--- auth.ts
+++ auth.ts
@@ -1,1 +1,33 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+ 
+const prisma = new PrismaClient();
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [ Google, GitHub, 
+        // Google({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string
+        // }), 
+        // GitHub({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string
+        // }),
+        CredentialsProvider({
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' }
+            },
+            async authorize(credentials, request) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Invalid credentials')
+                }
+            }
+        })
+    ],
+})
