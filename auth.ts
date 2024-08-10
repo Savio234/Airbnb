06b351789c18878dc922c943a3d38fc61d2d1a4e
Dominic <dominicezeifejafor@gmail.com>
2024-08-10 15:31:58 +0100

    Changes

--- auth.ts
+++ auth.ts
@@ -4,12 +4,14 @@ import GitHub from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
  
 const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [ Google, GitHub, 
+    providers: [ 
+        Google, GitHub,
         // Google({
         //     clientId: process.env.GITHUB_ID as string,
         //     clientSecret: process.env.GITHUB_SECRET as string
@@ -24,10 +26,40 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                 password: { label: 'password', type: 'password' }
             },
             async authorize(credentials, request) {
-                if (!credentials?.email || !credentials?.password) {
+                if ((!credentials?.email || typeof credentials?.email !== 'string') || 
+                    (!credentials?.password || typeof credentials?.password !== 'string')
+                ) {
                     throw new Error('Invalid credentials')
                 }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user || (user.hashedPassword || typeof user?.hashedPassword !== 'string')) {
+                    throw new Error('Invalid credentials')
+                }
+
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password, 
+                    user.hashedPassword
+                );
+                if (!isCorrectPassword) {
+                    throw new Error('Incorrect password')
+                }
+
+                return user
             }
         })
     ],
+    pages: {
+        signIn: '/'
+    },
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt'
+    },
+    secret: process.env.NEXTAUTH_SECRET
 })
