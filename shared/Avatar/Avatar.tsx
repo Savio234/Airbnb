c3bab1e99a03d0ef37682aa4404a8f3288f0321c
Dominic <dominicezeifejafor@gmail.com>
2025-01-08 13:34:25 +0100

    Changes

--- shared/Avatar/Avatar.tsx
+++ shared/Avatar/Avatar.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 const Avatar = () => {
+  const [currentURL, setCurrentURL] = useState<string>('')
+  useEffect(() => {
+		setCurrentURL(window.location.href);
+	}, []);
+  const cartPageLink = typeof window !== 'undefined' ? currentURL : '';
+
   return (
     <div className='relative rounded-full h-[2.4rem] w-[2.4rem]'>
         <Image alt='avatar' fill src='/svgs/placeholder.jpg' />
